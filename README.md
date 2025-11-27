8814c28717ef5468edfba20564b30d5edb034fa0
TaraG <guillorit.tara@gmail.com>
2025-11-27 17:39:24 +0100

    Update Projet.Rmd with working analysis code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TP_transcriptomique_CAFs
+
+
+Cancer-associated fibroblasts (CAFs) play an important role in the development of many tumors by secreting
+different soluble factors stimulating growth, vascularization, and inducing an immunosuppressive
+environment. They also induce a desmoplastic reaction remodeling the extracellular matrix. 
--- analyse_cafs.R
+++ analyse_cafs.R
@@ -1,11 +1,11 @@
----
-title: "Analyse CAF Seurat"
-output: html_document
----
-
-```{r setup, message=FALSE, warning=FALSE}
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 
+# Installation des packages nécessaires
+install.packages("dplyr")
+install.packages("Seurat")
+install.packages("future")
+
+# Chargement des bibliothèques
 library(dplyr)
 library(Seurat)
 library(future)
@@ -13,19 +13,17 @@ library(future)
 # Configuration pour Seurat
 plan(sequential)
 options(future.globals.maxSize = 10*1024^3)
-```
 
-```{r load_data}
+# Chargement des données
 cat("Chargement des données...\n")
 p5 <- read.csv("CAFs/GSM4805570_CountsMatrix_20G00953M_TN.txt.gz", sep = "\t")
 p4a <- read.csv("CAFs/GSM4805566_CountsMatrix_19G02977A_TN.txt.gz", sep = "\t")
 p4b <- read.csv("CAFs/GSM4805568_CountsMatrix_19G02977B_TN.txt.gz", sep = "\t")
 
 caf.data <- data.matrix(cbind(p5, p4a, p4b))
-cat("Données chargées:", dim(caf.data), "\n")
-```
+cat("Données chargées: ", dim(caf.data), "\n")
 
-```{r prepare_data}
+# Préparation des données
 cat("Préparation des données...\n")
 ens <- read.csv("ensembl-38-108-genes.txt", sep = "\t")
 
@@ -41,9 +39,8 @@ cat("Gènes retenus:", sum(good), "\n")
 symb <- symbols[good]
 caf.data <- caf.data[good,]
 rownames(caf.data) <- symb
-```
 
-```{r create_seurat}
+# Création de l'objet Seurat
 cat("Création de l'objet Seurat...\n")
 caf <- CreateSeuratObject(
   counts = caf.data,
@@ -54,4 +51,3 @@ caf <- CreateSeuratObject(
 
 print(caf)
 cat("Analyse terminée avec succès!\n")
-```
